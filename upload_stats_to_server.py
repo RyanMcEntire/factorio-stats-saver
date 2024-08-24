ce49fb4f3d7dcbabedcedf9e2e9c9de011ed8768
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-08-24 11:14:09 -0600

    add debounce time to prevent double uploads

--- upload_stats_to_server.py
+++ upload_stats_to_server.py
@@ -7,6 +7,7 @@ from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import logging
 import urllib3
+import time
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -31,10 +32,15 @@ class StatsFileHandler(FileSystemEventHandler):
     def __init__(self, file_path, upload_url):
         self.file_path = file_path
         self.upload_url = upload_url
+        self.last_modified = 0
+        self.debounce_time = 1
 
     def on_modified(self, event):
         if event.src_path == self.file_path:
-            self.upload_file()
+            current_time = time.time()
+            if current_time - self.last_modified > self.debounce_time:
+                self.last_modified = current_time
+                self.upload_file()
 
     def upload_file(self):
         try:
@@ -46,14 +52,19 @@ class StatsFileHandler(FileSystemEventHandler):
                     self.upload_url, json=data, headers=headers, verify=False
                 )
                 if response.status_code == 200:
+                    print"Uploaded data from {os.path.basename(self.file_path)}"()
                     logging.info(
                         f"Uploaded data from {os.path.basename(self.file_path)}"
                     )
                 else:
+                    print(
+                        "Failed to upload {os.path.basename(self.file_path)}. Status: {response.status_code}"
+                    )
                     logging.error(
                         f"Failed to upload {os.path.basename(self.file_path)}. Status: {response.status_code}"
                     )
         except Exception as e:
+            print("Error uploading {os.path.basename(self.file_path)}: {str(e)}")
             logging.error(
                 f"Error uploading {os.path.basename(self.file_path)}: {str(e)}"
             )
@@ -76,17 +87,21 @@ def main():
     observer.schedule(event_handler, path=os.path.dirname(stats_file), recursive=False)
     observer.start()
 
+    print("File watcher started")
     logging.info("File watcher started")
 
     try:
         while is_factorio_running():
             time.sleep(5)
+        print("Factorio stopped. Shutting down watcher")
         logging.info("Factorio stopped. Shutting down watcher")
     except KeyboardInterrupt:
+        print("Keyboard interrupt. Shutting down watcher")
         logging.info("Keyboard interrupt. Shutting down watcher")
     finally:
         observer.stop()
         observer.join()
+        print("File watcher stopped")
         logging.info("File watcher stopped")
 
 
