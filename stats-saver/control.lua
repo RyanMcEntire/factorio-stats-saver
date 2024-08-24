d2fe2697faee60843bb697414e426a410a3a2d56
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-08-23 22:54:58 -0600

    simplify mod and debug python script

--- stats-saver/control.lua
+++ stats-saver/control.lua
@@ -1,15 +1,3 @@
-local production_stats = {}
-local consumption_stats = {}
-
-local function initialize()
-  if not global.production_stats then
-    global.production_stats = {}
-  end
-  if not global.consumption_stats then
-    global.consumption_stats = {}
-  end
-end
-
 script.on_event(defines.events.on_tick, function(event)
   if event.tick % (60 * 60) == 0 then -- Every minute
     local force = game.forces["player"]
@@ -18,30 +6,13 @@ script.on_event(defines.events.on_tick, function(event)
     local new_production_stats = force.item_production_statistics.input_counts
     local new_consumption_stats = force.item_production_statistics.output_counts
 
-    local production_data = {}
-    local consumption_data = {}
+    local combined_data = {
+      tick = current_tick,
+      production = new_production_stats,
+      consumption = new_consumption_stats
+    }
 
-    for item, count in pairs(new_production_stats) do
-      if global.production_stats[item] ~= count then
-        global.production_stats[item] = count
-        table.insert(production_data, { tick = current_tick, item = item, count = count })
-      end
-    end
-
-    for item, count in pairs(new_consumption_stats) do
-      if global.consumption_stats[item] ~= count then
-        global.consumption_stats[item] = count
-        table.insert(consumption_data, { tick = current_tick, item = item, count = count })
-      end
-    end
-
-    if #production_data > 0 then
-      game.write_file("production_stats.json", game.table_to_json(production_data), false)
-    end
-
-    if #consumption_data > 0 then
-      game.write_file("consumption_stats.json", game.table_to_json(consumption_data), false)
-    end
+    game.write_file("stats.json", game.table_to_json(combined_data), false)
   end
 end)
 
--- upload_stats_to_server.py
+++ upload_stats_to_server.py
@@ -6,17 +6,23 @@ import psutil
 from watchdog.observers import Observer
 from watchdog.events import FileSystemEventHandler
 import logging
+import urllib3
 
-log_file = os.path.join(os.path.dirname(os.path.abspath(__file__)), 'factorio_stats_watcher.log')
+urllib3.disable_warnings(urllib3.exceptions.InsecureRequestWarning)
+
+log_file = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "factorio_stats_watcher.log"
+)
 logging.basicConfig(
     level=logging.INFO,
-    format='%(asctime)s - %(message)s',
-    datefmt='%Y-%m-%d %H:%M:%S',
+    format="%(asctime)s - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S",
     filename=log_file,
-    filemode='a'
+    filemode="a",
 )
 
-API_KEY = 'API_KEY_1'
+API_KEY = "TREEFISHPUNCH-9867-493f-b234-567890abcdef"
+
 
 class StatsFileHandler(FileSystemEventHandler):
     def __init__(self, file_path, upload_url):
@@ -29,34 +35,42 @@ class StatsFileHandler(FileSystemEventHandler):
 
     def upload_file(self):
         try:
-            with open(self.file_path, 'r') as file:
+            with open(self.file_path, "r") as file:
                 data = json.load(file)
-                headers = {'X-API-Key': API_KEY}
-                response = requests.post(self.upload_url, json=data, headers=headers)
+                headers = {"X-API-Key": API_KEY}
+                # REMOVE VERIFY=FALSE IN PRODUCTION
+                response = requests.post(
+                    self.upload_url, json=data, headers=headers, verify=False
+                )
                 if response.status_code == 200:
-                    logging.info(f"Uploaded data from {os.path.basename(self.file_path)}")
+                    logging.info(
+                        f"Uploaded data from {os.path.basename(self.file_path)}"
+                    )
                 else:
-                    logging.error(f"Failed to upload {os.path.basename(self.file_path)}. Status: {response.status_code}")
+                    logging.error(
+                        f"Failed to upload {os.path.basename(self.file_path)}. Status: {response.status_code}"
+                    )
         except Exception as e:
-            logging.error(f"Error uploading {os.path.basename(self.file_path)}: {str(e)}")
+            logging.error(
+                f"Error uploading {os.path.basename(self.file_path)}: {str(e)}"
+            )
+
 
 def is_factorio_running():
-    return 'factorio.exe' in (p.name().lower() for p in psutil.process_iter(['name']))
+    return "factorio.exe" in (p.name().lower() for p in psutil.process_iter(["name"]))
+
 
 def main():
     time.sleep(10)
-    
-    script_dir = os.path.dirname(os.path.abspath(__file__))
-    production_file = os.path.join(script_dir, '..', 'script-output', 'production_stats.json') 
-    consumption_file = os.path.join(script_dir, '..', 'script-output', 'consumption_stats.json')
-    server_url = 'https://localhost:3000/upload'
 
-    event_handler_prod = StatsFileHandler(production_file, server_url)
-    event_handler_cons = StatsFileHandler(consumption_file, server_url)
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    stats_file = os.path.join(script_dir, "..", "script-output", "stats.json")
+    server_url = "https://localhost:3000/upload"
+
+    event_handler = StatsFileHandler(stats_file, server_url)
 
     observer = Observer()
-    observer.schedule(event_handler_prod, path=os.path.dirname(production_file), recursive=False)
-    observer.schedule(event_handler_cons, path=os.path.dirname(consumption_file), recursive=False)
+    observer.schedule(event_handler, path=os.path.dirname(stats_file), recursive=False)
     observer.start()
 
     logging.info("File watcher started")
@@ -71,6 +85,7 @@ def main():
         observer.stop()
         observer.join()
         logging.info("File watcher stopped")
+
 
 if __name__ == "__main__":
     main()
