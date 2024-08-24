29a2fb09911888f967c2fabc70ca07a0fae03652
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-08-24 10:51:54 -0600

    add active mods and researched techs to mod output

--- stats-saver/control.lua
+++ stats-saver/control.lua
@@ -5,11 +5,21 @@ script.on_event(defines.events.on_tick, function(event)
 
     local new_production_stats = force.item_production_statistics.input_counts
     local new_consumption_stats = force.item_production_statistics.output_counts
+    local active_mods = game.active_mods
+    local researched_techs = {}
+
+    for tech_name, tech in pairs(force.technologies) do
+      if tech.researched then
+        table.insert(researched_techs, tech_name)
+      end
+    end
 
     local combined_data = {
       tick = current_tick,
       production = new_production_stats,
-      consumption = new_consumption_stats
+      consumption = new_consumption_stats,
+      research = researched_techs,
+      mods = active_mods
     }
 
     game.write_file("stats.json", game.table_to_json(combined_data), false)
