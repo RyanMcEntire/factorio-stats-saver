19c7106ee99dbe28d3749be71baf403caeb9ad0c
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-09-20 09:27:23 -0600

    add surface to game mod output

--- stats-saver/control.lua
+++ stats-saver/control.lua
@@ -1,29 +1,35 @@
 script.on_event(defines.events.on_tick, function(event)
-  if event.tick % (30 * 60) == 0 then --
-    local force = game.forces["player"]
-    local current_tick = event.tick
+	if event.tick % (30 * 60) == 0 then --
+		local force = game.forces["player"]
+		local current_tick = event.tick
 
-    local new_production_stats = force.item_production_statistics.input_counts
-    local new_consumption_stats = force.item_production_statistics.output_counts
-    local active_mods = game.active_mods
-    local researched_techs = {}
+		local surfaces = {}
+		local new_production_stats = force.item_production_statistics.input_counts
+		local new_consumption_stats = force.item_production_statistics.output_counts
+		local active_mods = game.active_mods
+		local researched_techs = {}
 
-    for tech_name, tech in pairs(force.technologies) do
-      if tech.researched then
-        table.insert(researched_techs, tech_name)
-      end
-    end
+		for name, surface in pairs(game.surfaces) do
+			table.insert(surfaces, name)
+		end
 
-    local combined_data = {
-      tick = current_tick,
-      production = new_production_stats,
-      consumption = new_consumption_stats,
-      research = researched_techs,
-      mods = active_mods
-    }
+		for tech_name, tech in pairs(force.technologies) do
+			if tech.researched then
+				table.insert(researched_techs, tech_name)
+			end
+		end
 
-    game.write_file("stats.json", game.table_to_json(combined_data), false)
-  end
+		local combined_data = {
+			surface = surfaces[1], -- TODO: This is hardcoded to one. Needs rewrite when DLC drops.
+			tick = current_tick,
+			production = new_production_stats,
+			consumption = new_consumption_stats,
+			research = researched_techs,
+			mods = active_mods,
+		}
+
+		game.write_file("stats.json", game.table_to_json(combined_data), false)
+	end
 end)
 
 script.on_init(initialize)
