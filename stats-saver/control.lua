3bc3a4e506a8cd7de47a0b8a83ca3cd7b386cc82
Ryan McEntire <107779145+RyanMcEntire@users.noreply.github.com>
2024-10-21 11:49:19 -0600

    syntax fixes for 2.0)

--- stats-saver/control.lua
+++ stats-saver/control.lua
@@ -3,8 +3,6 @@ script.on_event(defines.events.on_tick, function(event)
 		local force = game.forces["player"]
 		local current_tick = event.tick
 		local surfaces = {}
-		local new_production_stats = force.item_production_statistics.input_counts
-		local new_consumption_stats = force.item_production_statistics.output_counts
 		local active_mods = game.active_mods
 		local researched_techs = {}
 
@@ -14,18 +12,12 @@ script.on_event(defines.events.on_tick, function(event)
 			end
 		end
 
-		for name, id in pairs(game.surfaces) do
+		for name, _ in pairs(game.surfaces) do
 			surfaces[name] = {
 				production = get_item_production_statistics(name).input_counts,
 				consumption = get_item_production_statistics(name).output_counts,
 			}
 		end
-
-		-- NOTE:
-		-- Surface addition only adds for the one surface
-		-- until Space Age comes out.
-		-- Will need to iterate over each surface and get stats
-		-- when Space Age api is released.
 
 		local combined_data = {
 			tick = current_tick,
